3f3b218ec68d8958d19c0893413e7113abe4e4cd
Wokcito <maxikazanski@gmail.com>
2023-09-26 00:09:31 -0300

    added validation for repeated ranks
    resolves #4

--- src/helpers/query-validators.helper.ts
+++ src/helpers/query-validators.helper.ts
@@ -2,7 +2,14 @@ export function ranksValidator(ranks: string): boolean {
 	const RANKS = ['premium', 'black', 'platinum', 'gold', 'blue']
 	const ranksArray = ranks.split(',')
 
+	// Check ranks
 	if (ranksArray.some(rank => !RANKS.includes(rank))) { throw new Error('The ranks must be: \'premium\' | \'black\' | \'platinum\' | \'gold\' | \'blue\'') }
+
+	// Check repeated ranks
+	ranksArray.forEach(rank => {
+		const foundRanks = ranksArray.filter(foundRank => foundRank === rank)
+		if (foundRanks.length > 1) throw new Error('There are repeated ranks')
+	})
 
 	return true
 }
